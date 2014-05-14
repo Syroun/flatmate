925dfc8bcab19bb7e157c10a5b235d7c94eb89de
Marius <naryok@hotmail.de>
2014-05-14 13:05:34 +0200

    * Bider für Carsten, Peter und Silke zu den Datein hinzugefügt. * Die Farbe des Hintergrunds verändert(etwas dunkler) und die Farbe der Toolbar verändert. * Die Bilder in den Login eingearbeitet und alles nochmals angepasst (Name entfernt).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507095329) do
+ActiveRecord::Schema.define(:version => 20140507100353) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
